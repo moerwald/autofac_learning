c679b8afd1b7c740fac4de9bafc0ebb665bcf0a7
moerwald <andreas.mewald@gmail.com>
2018-02-15 19:23:29 +0100

    Add project files.

---
+++ DiWithoutContainer/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiWithoutContainer
+{
+
+    public interface ILog
+    {
+        void Write(string message);
+    }
+
+    public class ConsoleLog : ILog
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class Engine
+    {
+        private ILog log;
+        private int id;
+
+        public Engine(ILog log)
+        {
+            this.log = log;
+            this.id = new Random().Next();
+        }
+
+        public void Ahead(int power)
+        {
+            this.log.Write($"Engine [{id}] ahead {power}");
+        }
+    }
+
+
+    public class Car
+    {
+        private Engine engine;
+
+        private ILog log;
+
+        public Car(Engine engine, ILog log)
+        {
+            this.engine = engine;
+            this.log = log;
+        }
+
+        public void Go()
+        {
+            this.engine.Ahead(100);
+            this.log.Write("Car going forward ...");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = new ConsoleLog();
+            var engine = new Engine(log);
+            var car = new Car(engine, log);
+
+            car.Go();
+        }
+    }
+}
